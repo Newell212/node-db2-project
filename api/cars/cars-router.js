00d8f96b7c5895821000b99a437415e239ba2638
Michael Newell <michael.newell212@gmail.com>
2023-07-03 15:55:53 -0600

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const Car = require('./cars-model')
 const router = express.Router()
+const {
+checkCarId,
+checkCarPayload,
+checkVinNumberValid,
+checkVinNumberUnique
+} = require('./cars-middleware')
 
 router.get('/', async (req, res, next) => {
    try {
@@ -11,13 +17,8 @@ router.get('/', async (req, res, next) => {
    }
 })
 
-router.get('/:id', async (req, res, next) => {
-    try {
-        const car = await Car.getById(req.params.id)
-        res.json(car)
-       } catch(err) {
-        next(err)
-       }
+router.get('/:id', checkCarId, async (req, res, next) => {
+   res.json(req.car)
 })
 
 router.post('/', async (req, res, next) => {
