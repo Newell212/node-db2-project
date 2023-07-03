1188207fff38ac5f497ecfa31adf4bb9036d6db7
Michael Newell <michael.newell212@gmail.com>
2023-07-03 15:22:34 -0600

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -2,11 +2,11 @@ exports.up = function (knex) {
   return knex.schema.createTable('cars', tbl => {
     tbl.increments()
     tbl.string('vin', 17).notNullable().unique()
-    tbl.sting('make', 128).notNullable()
-    tbl.sting('model', 128).notNullable()
-    tbl.numeric('mileage').unsigned().notNullable()
-    tbl.sting('title', 128)
-    tbl.sting('transmission', 128)
+    tbl.string('make', 128).notNullable()
+    tbl.string('model', 128).notNullable()
+    tbl.integer('mileage').unsigned().notNullable()
+    tbl.string('title', 128)
+    tbl.string('transmission', 128)
   })
 };
 
